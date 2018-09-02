88168ddd27ca18fb73ac891e9bbacd415749b680
RJZ18 <42161041+RJZ18@users.noreply.github.com>
2018-09-02 13:19:10 -0500

    Update yellowPages.py

--- yellowPages.py
+++ yellowPages.py
@@ -11,6 +11,11 @@ class YPSpider(scrapy.Spider):
         for plumber in response.css('div[class="search-results organic"] div[class="result"] div[class="v-card"] div[class="info"]'):
             yield {
                 'COMPANY_NAME': plumber.css('h2[class="n"] a[class="business-name"] span::text').extract(),
-		'BBB_RATING': plumber.css('div[class="info-section info-primary"] span[class="bbb-rating extra-rating"]::text').extract()
+		'BBB_RATING': plumber.css('div[class="info-section info-primary"] span[class="bbb-rating extra-rating"]::text').extract(),
+		'STREET_ADDRESS': plumber.css('div[class="info-section info-primary"] p[class="adr"] span[class="street-address"]::text').extract(),
+		'CITY': plumber.css('div[class="info-section info-primary"] p[class="adr"] span[class="locality"]::text').extract(),
+		'STATE': plumber.css('div[class="info-section info-primary"] p[class="adr"] span[itemprop="addressRegion"]::text').extract(),
+		'POSTAL_CODE': plumber.css('div[class="info-section info-primary"] p[class="adr"] span[itemprop="postalCode"]::text').extract(),
+		'PHONE': plumber.css('div[class="info-section info-primary"] div[class="phones phone primary"]::text').extract()
             }
 
